b327ac40940e8701e6c39b8ebb6bf6dab77fdd6a
Fabio <fabio@fabiofarias.com.br>
2020-08-13 12:05:11 -0300

    OK

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -15,7 +15,7 @@ routes.get('/connections', conenctionsController.index);
 routes.post('/connections', conenctionsController.create);
 
 routes.get('/test', (request: Request, response: Response) => {
-    return response.json({'success': 'OK', 'message': 'Hello World'});
+    return response.json({'success': 'OKx', 'message': 'Hello World'});
 });
 
 export default routes;
